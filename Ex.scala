66a223fdceb9f62266b4ab5f946975440be29680
Alcides Fonseca <me@alcidesfonseca.com>
2016-07-09 00:46:29 +0100

    Using other references in transition

--- Ex.scala
+++ Ex.scala
@@ -4,17 +4,18 @@ class CellMemory extends CellMem {
 	var b:Float = 0;
 	
 	def transition {
-		a = a + 1;
+		a = Main.objects(0).a + Main.others(1).a + 1;
 	}
 }
 
 object Main {
+	
+	val objects = new Cell[CellMemory](20)
+	val others = new Cell[CellMemory](20)
+	
 	def main(args:Array[String]) {
-		
-		val objects = new Cell[CellMemory](20)
-		val others = new Cell[CellMemory](20)
 
-		MisoRuntime.runSeq(10,objects, others)
+		MisoRuntime.runSeq(10, objects, others)
 
 		println(others(1).a)
 	}
